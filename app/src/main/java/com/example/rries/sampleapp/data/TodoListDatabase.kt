5822351dbb7cdaec24bd951c920495c1fe237ef3
Riccarda Ries <riccarda.ries@namics.com>
2018-11-28 16:02:44 +0100

    Clean import

--- app/src/main/java/com/example/rries/sampleapp/data/TodoListDatabase.kt
+++ app/src/main/java/com/example/rries/sampleapp/data/TodoListDatabase.kt
@@ -1,14 +1,9 @@
 package com.example.rries.sampleapp.data
 
-import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.Context
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
-import kotlinx.coroutines.launch
 
 @Database(entities = [Todo::class], version = 1)
 abstract class TodoListDatabase: RoomDatabase() {
