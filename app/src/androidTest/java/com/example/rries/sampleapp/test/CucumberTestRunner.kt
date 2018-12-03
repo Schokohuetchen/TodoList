1cc18c19b7229676b7047fce317f19649b32a5cc
Riccarda Ries <riccarda.ries@namics.com>
2018-12-03 11:45:17 +0100

    Use correct path for steps

--- app/src/androidTest/java/com/example/rries/sampleapp/test/CucumberTestRunner.kt
+++ app/src/androidTest/java/com/example/rries/sampleapp/test/CucumberTestRunner.kt
@@ -2,8 +2,12 @@ package com.example.rries.sampleapp.test
 
 import android.os.Bundle
 import android.support.test.runner.MonitoringInstrumentation
+import cucumber.api.CucumberOptions
 import cucumber.api.android.CucumberInstrumentationCore
 
+@CucumberOptions(
+    features = ["features"],
+    glue = ["com.example.rries.sampleapp.test"])
 @Suppress("unused")
 class CucumberTestRunner: MonitoringInstrumentation() {
 
